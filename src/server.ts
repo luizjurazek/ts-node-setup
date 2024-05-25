d4cea2945d8d22113cdaac15c7c59cb94ffbc565
Luiz Jurazek <luizjurazek@gmail.com>
2024-05-25 14:09:52 -0300

    teste

---
+++ src/server.ts
@@ -0,0 +1,12 @@
+import * as express from "express";
+import { Request, Response } from "express";
+
+const app = express();
+
+app.get("/", (req: Request, res: Response) => {
+  res.send("Hello world!");
+});
+
+app.listen(3000, () => {
+  console.log("Listening on port 3000!");
+});
